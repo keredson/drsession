f60bbda885a0e594bcda9347fb9b2392c1054624
Derek Anderson <derek@hvst.com>
2016-07-11 17:32:30 -0500

    added dep on redis

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from distutils.core import setup
 from drsession import __version__
 
 setup(name='DRSession',
-      version=__version__,
-      description="Derek's Redis Session Middleware",
-      author='Derek Anderson',
-      author_email='public@kered.org',
-      url='https://github.com/keredson/drsession',
-      py_modules=['drsession'],
-     )
+  version=__version__,
+  description="Derek's Redis Session Middleware",
+  author='Derek Anderson',
+  author_email='public@kered.org',
+  url='https://github.com/keredson/drsession',
+  py_modules=['drsession'],
+  install_requires=['redis','Cookie'],
+)
 
